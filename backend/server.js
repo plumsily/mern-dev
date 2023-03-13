0e59963b5f1f7dd9c8c1ffbcd135c13b96beb8d2
Justin Lee <justnslee94@gmail.com>
2023-03-13 15:46:32 -0700

    Todos controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use("/api/todos", require("./routes/todoRoutes"));
+
+//Use custom error handling rather than default express handler
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on part ${port}`);
+});
